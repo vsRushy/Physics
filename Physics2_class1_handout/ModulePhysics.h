ed93c8740fbeb83b0e3c9ce0f0222509c20b8200
GerardMF21 <32986524+vsRushy@users.noreply.github.com>
2018-09-21 11:49:11 +0200

    Added Handout 1 files (still need to be finished)

---
+++ Physics2_class1_handout/ModulePhysics.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+#define METERS_TO_PIXELS( A ) A * 3779.5275590551      
+#define PIXELS_TO_METERS( A ) A / 3779.5275590551      
+
+class b2World;
+class b2Body;
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+private:
+	bool debug;
+	
+	b2World* world;
+	b2Body* groundBody;
+};
